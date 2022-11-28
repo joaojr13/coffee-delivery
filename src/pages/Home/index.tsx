984aa2d4e3f15a6103cc4495f78325d3b4f8a13f
João Jr <joao.jr13@icloud.com>
2022-11-28 00:34:25 -0300

    feat: desenvolvimento da seção de informações da página Home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,3 +1,44 @@
+import { Coffee, Package, ShoppingCart, Timer } from 'phosphor-react'
+import coffeeImage from '../../assets/coffee-home.svg'
+import { InfoContainer, ItemLogo, ItemsContainer } from './styles'
+
 export function Home() {
-  return <h1>Home</h1>
+  return (
+    <InfoContainer>
+      <div>
+        <h1>Encontre o café perfeito para qualquer hora do dia</h1>
+        <p>
+          Com o Coffee Delivery você recebe seu café onde estiver, a qualquer
+          hora.
+        </p>
+        <ItemsContainer>
+          <span>
+            <ItemLogo backgroundColor="yellow-dark">
+              <ShoppingCart size={16} weight="fill" color="#FFF" />
+            </ItemLogo>
+            Compra simples e segura
+          </span>
+          <span>
+            <ItemLogo backgroundColor="base-text">
+              <Package size={16} weight="fill" color="#FFF" />
+            </ItemLogo>
+            Embalagem mantém o café intacto
+          </span>
+          <span>
+            <ItemLogo backgroundColor="yellow">
+              <Timer size={16} weight="fill" color="#FFF" />
+            </ItemLogo>
+            Entrega rápida e rastreada
+          </span>
+          <span>
+            <ItemLogo backgroundColor="purple">
+              <Coffee size={16} weight="fill" color="#FFF" />
+            </ItemLogo>
+            O Café chega fresquinho até você
+          </span>
+        </ItemsContainer>
+      </div>
+      <img src={coffeeImage} alt="" />
+    </InfoContainer>
+  )
 }
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,1 +1,64 @@
 import styled from 'styled-components'
+import homeBackground from '../../assets/Background.svg'
+
+export const InfoContainer = styled.section`
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  padding-left: 160px;
+  padding-right: 160px;
+  height: 34rem;
+  gap: 56px;
+
+  background-image: url(${homeBackground});
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: top;
+
+  div {
+    width: 36rem;
+
+    h1 {
+      font-family: 'Baloo 2', cursive;
+      font-size: 3rem;
+      line-height: 1.3;
+      color: ${(props) => props.theme['base-title']};
+      margin-bottom: 16px;
+    }
+
+    p {
+      font-size: 1.25rem;
+      line-height: 1.3;
+      color: ${(props) => props.theme['base-subtitle']};
+    }
+  }
+`
+
+export const ItemsContainer = styled.div`
+  display: grid;
+  grid-template-columns: 231px 294px;
+  gap: 20px 40px;
+  margin-top: 67px;
+
+  span {
+    display: flex;
+    align-items: center;
+    gap: 12px;
+  }
+`
+
+export type LogoColor = 'yellow' | 'yellow-dark' | 'base-text' | 'purple'
+
+interface ItemLogoProps {
+  backgroundColor: LogoColor
+}
+
+export const ItemLogo = styled.span<ItemLogoProps>`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 50%;
+  width: 32px;
+  height: 32px;
+  background: ${(props) => props.theme[props.backgroundColor]};
+`
